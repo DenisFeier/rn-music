a44f5fa900e5dc1f142658966f590c7753816beb
DenisFeier <denis.feier@takeofflabs.com>
2021-09-30 18:40:46 +0300

    add firebase

--- App.tsx
+++ App.tsx
@@ -1,8 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5tT80vx6JOFynCvV9qiHPTQ3ssDrJZF4",
+  authDomain: "durable-destiny-275715.firebaseapp.com",
+  projectId: "durable-destiny-275715",
+  storageBucket: "durable-destiny-275715.appspot.com",
+  messagingSenderId: "280109639330",
+  appId: "1:280109639330:web:e635bd47f179e1625e59fb",
+  measurementId: "G-HKXXW5WLVM"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const dbh = firebase.firestore();
+
 export default function App() {
+
+  useEffect(() => {
+    dbh.collection("users").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const user = JSON.stringify(doc.data());
+        console.log(`${doc.id} => ${user}`);
+      });
+  });
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
